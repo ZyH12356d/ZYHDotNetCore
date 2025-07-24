5cbaadd173e29bdcc12d812b2b8d7d6d974f90d8
ZyH12356d <107683128+ZyH12356d@users.noreply.github.com>
2025-07-24 16:54:52 +0630

    feat : minimal API

---
+++ ZYHDotNetCore.MinimalAPI/Program.cs
@@ -0,0 +1,113 @@
+using Microsoft.EntityFrameworkCore;
+using ZYHDotNetCore.Database.AppDbContextModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+app.MapGet("/blogs", () =>
+{
+    AppDbContext dbContext = new AppDbContext();
+    var lst = dbContext.TblBlogs.ToList();
+    return Results.Ok(lst);
+});
+
+
+app.MapGet("/blogs/{id}", (int id) =>
+{
+    AppDbContext dbContext = new AppDbContext();
+    var item = dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+    if(item is null)
+    {
+        return Results.NotFound("No data");
+    }
+    return Results.Ok(item);
+});
+
+app.MapPost("/blogs", (TblBlog blog) =>
+{
+    AppDbContext dbContext = new AppDbContext();
+    dbContext.TblBlogs.Add(blog);
+    var result = dbContext.SaveChanges();
+    if (result > 0)
+    {
+        return Results.Ok(blog);
+    }
+    else
+    {
+        return Results.BadRequest("Failed to add blog.");
+    }
+});
+
+app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+{
+    AppDbContext dbContext = new AppDbContext();
+    var item = dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+
+    if (item is null)
+    {
+        return Results.NotFound("Blog not found or already deleted.");
+    }
+    
+    item.Title = blog.Title;
+    item.Author = blog.Author;
+    item.ContentData = blog.ContentData;
+    dbContext.Entry(item).State = EntityState.Modified;
+    //dbContext.TblBlogs.Update(item);
+    dbContext.SaveChanges();
+    return Results.Ok(item);
+
+});
+
+app.MapDelete("/blogs/{id}", (int id) =>
+{
+    AppDbContext dbContext = new AppDbContext();
+    var item = dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+    if (item is null)
+    {
+        return Results.NotFound("Blog not found or already deleted.");
+    }
+    item.DeleteFlag = 1; // Mark as deleted
+    dbContext.Entry(item).State = EntityState.Modified;
+    dbContext.SaveChanges();
+    return Results.Ok("Blog deleted successfully.");
+});
+app.Run();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
