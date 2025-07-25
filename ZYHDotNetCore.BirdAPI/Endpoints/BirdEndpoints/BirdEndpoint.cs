d915ec41324ac39761a8054392e8f09ff04c3794
ZyH12356d <107683128+ZyH12356d@users.noreply.github.com>
2025-07-25 16:30:40 +0630

    feat : minimal api (bird) with json data

---
+++ ZYHDotNetCore.BirdAPI/Endpoints/BirdEndpoints/BirdEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using ZYHDotNetCore.BirdAPI.Models.BirdModel;
+
+namespace ZYHDotNetCore.BirdAPI.Endpoints.BirdEndpoints
+{
+    public static class BirdEndpoint
+    {
+        public static void MapBirdEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/birds" , ()  =>
+            {
+                string filePath = "wwwroot/Data/Birds.json";
+                var jsonStr = File.ReadAllText(filePath);
+                var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr);
+                if (result == null || result.Tbl_Bird == null || result.Tbl_Bird.Length == 0)
+                {
+                    return Results.NotFound("No birds found.");
+                }
+                else
+                {
+                    return Results.Ok(result.Tbl_Bird);
+                }
+            });
+
+            app.MapGet("/birds/{id:int}", (int id) =>
+            {
+                string filePath = "wwwroot/Data/Birds.json";
+                var jsonStr = File.ReadAllText(filePath);
+                var items = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr);
+                var result = items.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+                if (result == null)
+                {
+                    return Results.NotFound($"Bird with ID {id} not found.");
+                }
+                else
+                {
+                    return Results.Ok(result);
+                }
+            });
+        }
+    }
+}
---
+++ ZYHDotNetCore.BirdAPI/Models/BirdModel/BirdModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ZYHDotNetCore.BirdAPI.Models.BirdModel;
+
+public class BirdResponseModel
+{
+    public Tbl_Bird[] Tbl_Bird { get; set; }
+}
+
+public class Tbl_Bird
+{
+    public int Id { get; set; }
+    public string BirdMyanmarName { get; set; }
+    public string BirdEnglishName { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}    
