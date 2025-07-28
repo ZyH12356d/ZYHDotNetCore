9b1b40084d88f0de7023536b3f0056caec0be153
ZyH12356d <107683128+ZyH12356d@users.noreply.github.com>
2025-07-28 22:09:26 +0630

    feat : User Register Endpoint

--- ZYHDotNetCore.Domain/Features/Blog/BlogServices.cs
+++ ZYHDotNetCore.Domain/Features/Blog/BlogServices.cs
@@ -6,59 +6,58 @@ using System.Text;
 using System.Threading.Tasks;
 using ZYHDotNetCore.Database.AppDbContextModels;
 
-namespace ZYHDotNetCore.Domain.Features.Blog
+namespace ZYHDotNetCore.Domain.Features.Blog;
+
+public class BlogServices
 {
-    public class BlogServices
+    private readonly AppDbContext _dbContext = new AppDbContext();
+    public List<TblBlog> GetBlog()
     {
-        private readonly AppDbContext _dbContext = new AppDbContext();
-        public List<TblBlog> GetBlog()
+        var lst = _dbContext.TblBlogs.ToList();
+        return lst;
+    }
+    public TblBlog GetBlogById(int id)
+    {
+        var item = _dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+        return item;
+    }
+    public TblBlog CreateBlog(TblBlog blog)
+    {
+        _dbContext.TblBlogs.Add(blog);
+        var result = _dbContext.SaveChanges();
+        if (result > 0)
         {
-            var lst = _dbContext.TblBlogs.ToList();
-            return lst;
+            return blog;
         }
-        public TblBlog GetBlogById(int id)
+        else
         {
-            var item = _dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
-            return item;
+            throw new Exception("Failed to add blog.");
         }
-        public TblBlog CreateBlog(TblBlog blog)
+    }
+    public TblBlog EditBlog(int id, TblBlog blog)
+    {
+        var item = _dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+        if (item is null)
         {
-            _dbContext.TblBlogs.Add(blog);
-            var result = _dbContext.SaveChanges();
-            if (result > 0)
-            {
-                return blog;
-            }
-            else
-            {
-                throw new Exception("Failed to add blog.");
-            }
+            throw new Exception("Blog not found or already deleted.");
         }
-        public TblBlog EditBlog(int id, TblBlog blog)
+        item.Title = blog.Title;
+        item.Author = blog.Author;
+        item.ContentData = blog.ContentData;
+        _dbContext.Entry(item).State = EntityState.Modified;
+        _dbContext.SaveChanges();
+        return item;
+    }
+    public bool DeleteBlog(int id)
+    {
+        var item = _dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+        if (item is null)
         {
-            var item = _dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
-            if (item is null)
-            {
-                throw new Exception("Blog not found or already deleted.");
-            }
-            item.Title = blog.Title;
-            item.Author = blog.Author;
-            item.ContentData = blog.ContentData;
-            _dbContext.Entry(item).State = EntityState.Modified;
-            _dbContext.SaveChanges();
-            return item;
+            return false;
         }
-        public bool DeleteBlog(int id)
-        {
-            var item = _dbContext.TblBlogs.AsNoTracking().FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
-            if (item is null)
-            {
-                return false;
-            }
-            item.DeleteFlag = 1; // Mark as deleted
-            _dbContext.Entry(item).State = EntityState.Modified;
-            _dbContext.SaveChanges();
-            return true;
-        }
+        item.DeleteFlag = 1; // Mark as deleted
+        _dbContext.Entry(item).State = EntityState.Modified;
+        _dbContext.SaveChanges();
+        return true;
     }
 }
---
+++ ZYHDotNetCore.MiniTransfer/EndPoints/UserRegisterEndpoints/UserRegisterEndpoint.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using ZYHDotNetCore.Database.AppDbContextModels;
+
+namespace ZYHDotNetCore.MiniTransfer.EndPoints.UserRegisterEndpoints
+{
+    public static class UserRegisterEndpoint
+    {
+        public static void MapUserRegisterEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/user", () =>
+            {
+                AppDbContext db = new AppDbContext();
+                var lst = db.TblUsers.Where(x => x.DeleteFlag == 0).ToList();
+                return Results.Ok(lst);
+            })
+            .WithName("UserRegisterList")
+            .WithTags("User")
+            .WithOpenApi();
+
+            app.MapGet("/api/user/{id}", (int id) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var result = db.TblUsers.FirstOrDefault(x => x.Id == id && x.DeleteFlag == 0);
+                if(result is null)
+                {
+                    return Results.NotFound("User Not Found");
+                }
+                else
+                {
+                    return Results.Ok(result);
+                }
+            })
+            .WithName("UserRegisterListById")
+            .WithTags("User")
+            .WithOpenApi();
+
+            app.MapPost("/api/user/register", (TblUser user) =>
+            {
+                AppDbContext db = new AppDbContext();
+                db.TblUsers.Add(user);
+                var result = db.SaveChanges();
+                if (result is 1)
+                {
+                    return Results.Created($"/api/user/{user.Id}", user);
+                }
+                else
+                {
+                    return Results.BadRequest();
+                }
+            })
+            .WithName("UserRegister")
+            .WithTags("User")
+            .WithOpenApi();
+        }
+    }
+}
